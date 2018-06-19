757e112b8290a2c6e182eb4c0411af37ad687dea
Bryson Kruk <brysonmk1984@gmail.com>
2018-06-19 11:31:56 -0600

    webpack now working on dev server and prod build

--- app/src/Components/Footer.js
+++ app/src/Components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer>My footer</footer>
+    <footer>
+        <div>
+            <strong>&copy; Bryson Kruk Design & Development</strong><br />
+            <a href="htttp://brysonkruk.com" >www.brysonkruk.com</a>
+        </div>
+    </footer>
 );
 
 export default Footer;
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,45 +1,57 @@
 const path = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    'webpack-hot-middleware/client?reload=true',
-    './main.js',
-  ],
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader"
-        ]
-      },
-      {
-        test: /\.(png|gif|jpg)$/,
-        exclude: /node_modules/,
-        use: [
-          'file-loader'
-        ],
-      },
+
+module.exports = env => {
+  if(typeof env === 'undefined'){env = 'development'}
+  return {
+    context: path.join(__dirname, 'src'),
+    entry: [
+      'webpack-hot-middleware/client?reload=true',
+      './main.js',
     ],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  mode: 'development'
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+          ],
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+              env.development ? "style-loader" : MiniCssExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+          ]
+        },
+        {
+          test: /\.(png|gif|jpg)$/,
+          exclude: /node_modules/,
+          //loader : 'file-loader?name=[name]-[hash:6].[ext]&outputPath=assets/images/'
+          use : ['file-loader?name=[name]-[hash:6].[ext]&outputPath=assets/images/']
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({filename:'index.html', template: 'index.html'}),
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin({
+
+        filename: env.development ?'assets/css/[name].css' : 'assets/css/[name].[hash].css'
+      })
+    ],
+    mode: env.development ? 'development' : 'production'
+  }
 };
