bdfe3e95c658c68214f4ebb9998f239df3e2d24b
Bryson Kruk <brysonmk1984@gmail.com>
2018-06-20 06:59:12 -0600

    added fonts

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -56,9 +56,16 @@ module.exports = env => {
         {
           test: /\.(png|gif|jpg)$/,
           exclude: /node_modules/,
-          //loader : 'file-loader?name=[name]-[hash:6].[ext]&outputPath=assets/images/'
           use : ['file-loader?name=[name]-[hash:6].[ext]&outputPath=assets/images/']
         },
+        { test: /\.(woff)(\?[a-z0-9]+)?$/, loader: 'file-loader?name=[name].[ext]&publicPath=../../&outputPath=assets/font/' },
+        //{ test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=[name]-[hash:6].[ext]&outputPath=assets/fonts/' },
+        //{ test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=[name]-[hash:6].[ext]&outputPath=assets/fonts/' },
+        // {
+        //   test: /\.(eot|svg|ttf|woff|woff2)$/,
+        //   exclude: /node_modules/,
+        //   use : ['file-loader?name=[name]-[hash:6].[ext]&outputPath=assets/fonts/']
+        // },
       ],
     },
     plugins,
