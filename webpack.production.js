7a59b10d2d8e1b49430743ab78c623525d986305
Bryson Kruk <brysonmk1984@gmail.com>
2018-08-13 17:22:11 -0600

    switched to webpack dev server, separated dev and prod wp files

---
+++ webpack.production.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const plugins = [
+  new CleanWebpackPlugin(['dist']),
+  new HtmlWebpackPlugin({filename:'index.html', template: 'index.html'}),
+  new MiniCssExtractPlugin({filename: 'assets/css/[name].[hash].css'})
+];
+
+
+module.exports = {
+
+    context: path.join(__dirname, 'src'),
+    entry: [
+      './main.js',
+    ],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath : '/',
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+          ],
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+          ]
+        },
+        {
+          test: /\.(png|gif|jpg)$/,
+          exclude: /node_modules/,
+          use : ['file-loader?name=[name].[ext]&outputPath=assets/images/'],
+          include: path.join(__dirname, 'src')
+        },
+        {
+          test: /\.json$/,
+          include: '/build/contracts/EcoAllyBase.json',
+          use : ['json-loader']
+          
+        },
+        { test: /\.(woff)(\?[a-z0-9]+)?$/, loader: 'file-loader?name=[name].[ext]&publicPath=../../&outputPath=assets/font/' },
+      ],
+    },
+    plugins,
+    resolve: {
+      alias: {
+         '~' : path.resolve( __dirname, 'src' )
+      },
+    },
+    mode: 'production'
+};
