b6767e5f71a935f466cd5c4e3d2c3e715756666b
Bryson Kruk <brysonmk1984@gmail.com>
2018-12-14 14:27:56 -0700

    added kos, moved images

--- src/common/crackDna.js
+++ src/common/crackDna.js
@@ -1,19 +1,3 @@
-
-import Fred from '~/assets/images/fred.png';
-import Dave from '~/assets/images/sherrif.png';
-import Xin from '~/assets/images/ninja.png';
-
-import filter_bot from '~/assets/images/filter_bot.png';
-import solar_sprite from '~/assets/images/solar_sprite.png';
-import geothermal_golem from '~/assets/images/geothermal_golem.png';
-import compost_creature from '~/assets/images/compost_creature.png';
-import chrono_guy from '~/assets/images/chrono_guy.png';
-import natural_ninja from '~/assets/images/natural_ninja.png';
-import timber_terror from '~/assets/images/timber_terror.png';
-import wild_speaker from '~/assets/images/wild_speaker.png';
-import the_empath from '~/assets/images/the_empath.png';
-import reuse_bot from '~/assets/images/reuse_bot.png';
-
 import allyList from './allyList.json';
 import * as AllyImages from '~/common/includedImages';
 import { lowercaseUnderscore } from '~/common/helperFunctions';
--- src/common/includedImages.js
+++ src/common/includedImages.js
@@ -1,19 +1,27 @@
+import filter_bot from '~/assets/images/allies/filter_bot/filter_bot.png';
+import solar_sprite from '~/assets/images/allies/solar_sprite/solar_sprite.png';
+import geothermal_golem from '~/assets/images/allies/geothermal_golem/geothermal_golem.png';
+import compost_creature from '~/assets/images/allies/compost_creature/compost_creature.png';
+import chrono_guy from '~/assets/images/allies/chrono_guy/chrono_guy.png';
+import natural_ninja from '~/assets/images/allies/natural_ninja/natural_ninja.png';
+import timber_terror from '~/assets/images/allies/timber_terror/timber_terror.png';
+import wild_speaker from '~/assets/images/allies/wild_speaker/wild_speaker.png';
+import the_empath from '~/assets/images/allies/the_empath/the_empath.png';
+import reuse_bot from '~/assets/images/allies/reuse_bot/reuse_bot.png';
 
-import Fred from '~/assets/images/fred.png';
-import Dave from '~/assets/images/sherrif.png';
-import Xin from '~/assets/images/ninja.png';
 
-import filter_bot from '~/assets/images/filter_bot.png';
-import solar_sprite from '~/assets/images/solar_sprite.png';
-import geothermal_golem from '~/assets/images/geothermal_golem.png';
-import compost_creature from '~/assets/images/compost_creature.png';
-import chrono_guy from '~/assets/images/chrono_guy.png';
-import natural_ninja from '~/assets/images/natural_ninja.png';
-import timber_terror from '~/assets/images/timber_terror.png';
-import wild_speaker from '~/assets/images/wild_speaker.png';
-import the_empath from '~/assets/images/the_empath.png';
-import reuse_bot from '~/assets/images/reuse_bot.png';
+import filter_bot_ko from '~/assets/images/allies/filter_bot/filter_bot.png';
+import solar_sprite_ko from '~/assets/images/allies/solar_sprite/solar_sprite.png';
+import geothermal_golem_ko from '~/assets/images/allies/geothermal_golem/geothermal_golem.png';
+import compost_creature_ko from '~/assets/images/allies/compost_creature/compost_creature.png';
+import chrono_guy_ko from '~/assets/images/allies/chrono_guy/chrono_guy.png';
+import natural_ninja_ko from '~/assets/images/allies/natural_ninja/natural_ninja.png';
+import timber_terror_ko from '~/assets/images/allies/timber_terror/timber_terror_ko.png';
+import wild_speaker_ko from '~/assets/images/allies/wild_speaker/wild_speaker.png';
+import the_empath_ko from '~/assets/images/allies/the_empath/the_empath.png';
+import reuse_bot_ko from '~/assets/images/allies/reuse_bot/reuse_bot.png';
 
-import timber_terror_ko from '~/assets/images/allies/timber_terror_ko.png';
-
-export {filter_bot,solar_sprite,geothermal_golem,compost_creature,chrono_guy,natural_ninja,timber_terror,wild_speaker,the_empath,reuse_bot, timber_terror_ko};
+export {
+  filter_bot,solar_sprite,geothermal_golem,compost_creature,chrono_guy,natural_ninja,timber_terror,wild_speaker,the_empath,reuse_bot,
+  filter_bot_ko,solar_sprite_ko,geothermal_golem_ko,compost_creature_ko,chrono_guy_ko,natural_ninja_ko,timber_terror_ko,wild_speaker_ko,the_empath_ko,reuse_bot_ko
+};
