2a06d7b8a6ac369acf5e3e14de8c679c34d4f777
Bryson Kruk <brysonmk1984@gmail.com>
2018-12-16 15:15:10 -0700

    added golem and reuse bot modals

--- src/common/includedBackgrounds.js
+++ src/common/includedBackgrounds.js
@@ -1,14 +1,14 @@
-// import filter_bot_bg from '~/assets/images/allies/filter_bot/filter_bot_bg.jpg';
-// import solar_sprite_bg from '~/assets/images/allies/solar_sprite/solar_sprite_bg.jpg';
-// import geothermal_golem_bg from '~/assets/images/allies/geothermal_golem/geothermal_golem_bg.jpg';
-// import compost_creature_bg from '~/assets/images/allies/compost_creature/compost_creature_bg.jpg';
+// import filter_bot from '~/assets/images/allies/filter_bot/filter_bot_bg.jpg';
+// import solar_sprite from '~/assets/images/allies/solar_sprite/solar_sprite_bg.jpg';
+ import geothermal_golem from '~/assets/images/allies/geothermal_golem/geothermal_golem_bg.jpg';
+// import compost_creature from '~/assets/images/allies/compost_creature/compost_creature_bg.jpg';
  import chrono_guy from '~/assets/images/allies/chrono_guy/chrono_guy_bg.jpg';
-// import natural_ninja_bg from '~/assets/images/allies/natural_ninja/natural_ninja_bg.jpg';
+// import natural_ninja from '~/assets/images/allies/natural_ninja/natural_ninja_bg.jpg';
  import timber_terror from '~/assets/images/allies/timber_terror/timber_terror_bg.jpg';
-// import wild_speaker_bg from '~/assets/images/allies/wild_speaker/wild_speaker_bg.jpg';
-// import the_empath_bg from '~/assets/images/allies/the_empath/the_empath_bg.jpg';
-// import reuse_bot_bg from '~/assets/images/allies/reuse_bot/reuse_bot_bg.jpg';
+// import wild_speaker from '~/assets/images/allies/wild_speaker/wild_speaker_bg.jpg';
+// import the_empath from '~/assets/images/allies/the_empath/the_empath_bg.jpg';
+ import reuse_bot from '~/assets/images/allies/reuse_bot/reuse_bot_bg.jpg';
 
 export {
-  chrono_guy,timber_terror
+  geothermal_golem, chrono_guy, timber_terror, reuse_bot
 };
