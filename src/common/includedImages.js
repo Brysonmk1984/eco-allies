42bb08d11e6cc77c64575170b44b88b997f064c9
Bryson Kruk <brysonmk1984@gmail.com>
2019-04-13 19:28:32 -0600

    changed filtron name, new art

--- src/common/includedImages.js
+++ src/common/includedImages.js
@@ -1,4 +1,4 @@
-import filter_bot from '~/assets/images/allies/filter_bot/filter_bot.png';
+import filtron_5 from '~/assets/images/allies/filtron_5/filtron_5.png';
 import solar_sprite from '~/assets/images/allies/solar_sprite/solar_sprite.png';
 import boulder_bro from '~/assets/images/allies/boulder_bro/boulder_bro.png';
 import compost_creature from '~/assets/images/allies/compost_creature/compost_creature.png';
@@ -9,7 +9,7 @@ import wild_speaker from '~/assets/images/allies/wild_speaker/wild_speaker.png';
 import the_empath from '~/assets/images/allies/the_empath/the_empath.png';
 import reuse_bot from '~/assets/images/allies/reuse_bot/reuse_bot.png';
 
-import filter_bot_ns from '~/assets/images/allies/filter_bot/filter_bot_ns.png';
+import filtron_5_ns from '~/assets/images/allies/filtron_5/filtron_5_ns.png';
 import solar_sprite_ns from '~/assets/images/allies/solar_sprite/solar_sprite_ns.png';
 import boulder_bro_ns from '~/assets/images/allies/boulder_bro/boulder_bro_ns.png';
 import compost_creature_ns from '~/assets/images/allies/compost_creature/compost_creature_ns.png';
@@ -20,7 +20,7 @@ import wild_speaker_ns from '~/assets/images/allies/wild_speaker/wild_speaker_ns
 import the_empath_ns from '~/assets/images/allies/the_empath/the_empath_ns.png';
 import reuse_bot_ns from '~/assets/images/allies/reuse_bot/reuse_bot_ns.png';
 
-import filter_bot_ko from '~/assets/images/allies/filter_bot/filter_bot_ko.png';
+import filtron_5_ko from '~/assets/images/allies/filtron_5/filtron_5_ko.png';
 import solar_sprite_ko from '~/assets/images/allies/solar_sprite/solar_sprite_ko.png';
 import boulder_bro_ko from '~/assets/images/allies/boulder_bro/boulder_bro_ko.png';
 import compost_creature_ko from '~/assets/images/allies/compost_creature/compost_creature_ko.png';
@@ -33,7 +33,7 @@ import reuse_bot_ko from '~/assets/images/allies/reuse_bot/reuse_bot_ko.png';
 
 
 export {
-  filter_bot,solar_sprite,boulder_bro,compost_creature,chrono_guy,natural_ninja,timber_terror,wild_speaker,the_empath,reuse_bot,
-  filter_bot_ko,solar_sprite_ko,boulder_bro_ko,compost_creature_ko,chrono_guy_ko,natural_ninja_ko,timber_terror_ko,wild_speaker_ko,the_empath_ko,reuse_bot_ko,
-  filter_bot_ns,solar_sprite_ns,boulder_bro_ns,compost_creature_ns,chrono_guy_ns,natural_ninja_ns,timber_terror_ns,wild_speaker_ns,the_empath_ns,reuse_bot_ns
+  filtron_5,solar_sprite,boulder_bro,compost_creature,chrono_guy,natural_ninja,timber_terror,wild_speaker,the_empath,reuse_bot,
+  filtron_5_ko,solar_sprite_ko,boulder_bro_ko,compost_creature_ko,chrono_guy_ko,natural_ninja_ko,timber_terror_ko,wild_speaker_ko,the_empath_ko,reuse_bot_ko,
+  filtron_5_ns,solar_sprite_ns,boulder_bro_ns,compost_creature_ns,chrono_guy_ns,natural_ninja_ns,timber_terror_ns,wild_speaker_ns,the_empath_ns,reuse_bot_ns
 };
