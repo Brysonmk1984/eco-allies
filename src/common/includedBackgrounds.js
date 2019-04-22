8e648d35eb3ccef934374b87364d0f1f0afe1ef5
Bryson Kruk <brysonmk1984@gmail.com>
2019-04-22 09:21:55 -0600

    fixed background path

--- src/common/includedBackgrounds.js
+++ src/common/includedBackgrounds.js
@@ -6,9 +6,9 @@
  import natural_ninja from '~/assets/images/allies/natural_ninja/natural_ninja_bg.jpg';
  import timber_terror from '~/assets/images/allies/timber_terror/timber_terror_bg.jpg';
  import wild_speaker from '~/assets/images/allies/wild_speaker/wild_speaker_bg.jpg';
- import the_empath from '~/assets/images/allies/the_empath/the_empath_bg.jpg';
+ import empath_aurelia from '~/assets/images/allies/empath_aurelia/empath_aurelia_bg.jpg';
  import reuse_bot from '~/assets/images/allies/reuse_bot/reuse_bot_bg.jpg';
 
 export {
-  filtron_5, solar_sprite, boulder_bro, compost_creature, chrono_guy, natural_ninja, timber_terror, wild_speaker, the_empath, reuse_bot
+  filtron_5, solar_sprite, boulder_bro, compost_creature, chrono_guy, natural_ninja, timber_terror, wild_speaker, empath_aurelia, reuse_bot
 };
